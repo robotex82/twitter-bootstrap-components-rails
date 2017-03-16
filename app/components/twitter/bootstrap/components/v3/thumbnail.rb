07a96e1ebdef40094b99a986712917b2e58e2848
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-03-17 00:48:41 +0100

    Add v3 thumbnail component.

---
+++ app/components/twitter/bootstrap/components/v3/thumbnail.rb
@@ -0,0 +1,40 @@
+module Twitter
+  module Bootstrap
+    module Components
+      module V3
+        class Thumbnail < Base
+          DEFAULT_IMAGE_SRC = "https://placeholdit.imgix.net/~text?txtsize=16&txt=242x200&w=242&h=200".freeze
+          DEFAULT_CONTAINER_CLASSES = "col-xs-6 col-md-3".freeze
+
+          private
+
+          def view_locals
+            { 
+              block_output:      @block_output,
+              context:           context,
+              container_classes: container_classes,
+              image_options:     image_options,
+              link_target:       link_target
+            }
+          end
+
+          def context
+            @options[:context]
+          end
+
+          def container_classes
+             @container_classes ||= @options[:container_classes] ||= DEFAULT_CONTAINER_CLASSES
+          end
+
+          def image_options
+            @image_options ||= (@options[:image_options] || {}).reverse_merge!(alt: nil, src: DEFAULT_IMAGE_SRC)
+          end
+
+          def link_target
+            @options[:link_target] || "#"
+          end
+        end
+      end
+    end
+  end
+end
--- app/helpers/twitter/bootstrap/components/rails/v3/components_helper.rb
+++ app/helpers/twitter/bootstrap/components/rails/v3/components_helper.rb
@@ -15,6 +15,10 @@ module Twitter
             def bootstrap_media_object(options = {}, &block)
               Twitter::Bootstrap::Components::V3::MediaObject.new(self, options, &block).perform
             end
+
+            def bootstrap_thumbnail(options = {}, &block)
+              Twitter::Bootstrap::Components::V3::Thumbnail.new(self, options, &block).perform
+            end
           end
         end
       end
