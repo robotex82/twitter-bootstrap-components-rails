060af42a08eb6412f62e233e1716f235312a397f
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-18 15:14:36 +0100

    V3::Thumbnail: Add possibility ot pass css classes to the thumbnail div.

--- app/components/twitter/bootstrap/components/v3/thumbnail.rb
+++ app/components/twitter/bootstrap/components/v3/thumbnail.rb
@@ -2,9 +2,14 @@ module Twitter
   module Bootstrap
     module Components
       module V3
+        # Example with active storage:
+        #
+        #     = bootstrap_thumbnail(image_options: { src: main_app.url_for(picture.asset) })
+        #
         class Thumbnail < Base
           DEFAULT_IMAGE_SRC = "https://placeholdit.imgix.net/~text?txtsize=16&txt=242x200&w=242&h=200".freeze
           DEFAULT_CONTAINER_CLASSES = "col-xs-6 col-md-3".freeze
+          DEFAULT_THUMBNAIL_CLASSES = "thumbnail".freeze
 
           private
 
@@ -14,6 +19,7 @@ module Twitter
               context:           context,
               container_id:      container_id,
               container_classes: container_classes,
+              thumbnail_classes: thumbnail_classes,
               image_options:     image_options,
               link_options:      link_options
             }
@@ -29,6 +35,10 @@ module Twitter
 
           def container_classes
              @options[:container_classes] || DEFAULT_CONTAINER_CLASSES
+          end
+
+          def thumbnail_classes
+             @options[:thumbnail_classes] || DEFAULT_THUMBNAIL_CLASSES
           end
 
           def image_options
